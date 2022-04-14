6f45b4cbf650619986f100ceb0459132421cd46f
Erika Dias <99773814+ErikaDIAS1@users.noreply.github.com>
2022-04-13 23:51:53 -0300

    Exercício 11

---
+++ Atividade 11.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+# include <locale.h>
+
+using namespace std;
+int main ()
+    {
+        float lado, area;
+
+        cout << "Digite o valor do lado do quadrado: " << endl;
+        cin >> lado;
+
+        area = lado * lado;
+
+        cout << "O valor da area do quadrado vale: " << area << endl;
+}
